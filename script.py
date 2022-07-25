9a259d98c07ee7b85c2cec2e4dcb47a4c7128d8d
Matthew Dowse <dowsema@tcd.ie>
2022-07-25 20:30:17 +0100

    Update script.py
    Script returns username from the personal token.
    Note I have removed the token and put in "token" as a place holder for now.

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("Test script works a OK!");
+#this script accesses the GitHub API
+
+from github import Github
+
+##insert token. Will need to change later to args
+# I have put "token" here to not leave any trace of personal tokens
+g = Github("token") 
+
+user = g.get_user()
+print("user: " + user.login)
