d01f5915711b9b40f230476dda2c72f94cc1682f
Matthew Dowse <dowsema@tcd.ie>
2022-07-26 13:36:24 +0100

    Multiple data taken from GitHub
    Will now work if the location field is empty on a GitHub profile.

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #this script accesses the GitHub API
 
+#import Github from the PyGithub library
 from github import Github
 
 ##insert token. Will need to change later to args
@@ -7,4 +8,11 @@ from github import Github
 g = Github("token") 
 
 user = g.get_user()
+
 print("user: " + user.login)
+
+#will now work if location field is empty on GitHub profile
+if user.location is not None:
+     print("location: " + user.location)
+
+
