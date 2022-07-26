56f28e80715cc232f3f27b31669cbb022e8943c3
Matthew Dowse <dowsema@tcd.ie>
2022-07-26 21:20:41 +0100

    Update script.py
    Token is now taken from command line.
    Null fields are removed when storing
    Data is stored in a Mongo Database.

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 #this script accesses the GitHub API
 
 #import Github from the PyGithub library
-from github import Github
-import json 
+from github import Github   #GitHub API access
+import json                 
+import pymongo  
 
 ##insert token. Will need to change later to args
 # I have put "token" here to not leave any trace of personal tokens
-g = Github("token") 
+token = input("Paste your token to continue ")
+g = Github(token) 
 
 user = g.get_user()
 
@@ -20,4 +22,24 @@ dct = { 'user': user.login, 'location': user.location}
 
 print ("dictionary is " + json.dumps(dct))
 
+
+#Storing dictionary into a Mongo Database
+
+#Remove null fields
+for k, v in dict(dct).items():
+    if v is None:
+        del dct[k]
+        
+print("Cleaned dictionary is " + json.dumps(dct))
+
+#Establish connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+#Create a Database
+db = client.classDB
+
+db.githubuser.insert_many([dct])
+
+                
 
