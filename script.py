d7f6e9230fbb691c258817867a7d74323b1b67ff
Matthew Dowse <dowsema@tcd.ie>
2022-07-27 15:56:54 +0100

    Anonymity of Data using Faker

--- script.py
+++ script.py
@@ -4,11 +4,18 @@
 from github import Github   #GitHub API access
 import json                 
 import pymongo  
+import os
+
+from faker import Faker
+from collections import defaultdict
+faker = Faker()
+names = defaultdict(faker.name)
 
 ##insert token. Will need to change later to args
 # I have put "token" here to not leave any trace of personal tokens
 token = input("Paste your token to continue ")
-g = Github(token) 
+#token = os.getenv('GITHUB_PAT')
+g = Github(token)
 
 user = g.get_user()
 
@@ -18,7 +25,12 @@ user = g.get_user()
 # if user.location is not None:
 #      print("location: " + user.location)
 
-dct = { 'user': user.login, 'location': user.location}
+dct = {'user': names[user.login].replace(" ", ""), 
+       'fullname' : names[user.name],
+       'location': user.location,
+       'company': user.company,
+       'public_repos': user.public_repos
+       }
 
 print ("dictionary is " + json.dumps(dct))
 
@@ -41,5 +53,23 @@ db = client.classDB
 
 db.githubuser.insert_many([dct])
 
-                
+##demo
 
+followercount = user.followers
+print("followers: " + str(followercount))
+
+followercount = user.get_followers()
+
+for f in followercount:
+    dct = {'user': names[f.login].replace(" ", ""),
+           'fullname': names[f.name],
+           'location': f.location,
+           'company': f.company,
+           'public_repos': f.public_repos
+          }
+    for k, v in dict(dct).items():
+        if v is None:
+            del dct[k]
+                
+    print("follower: " + json.dumps(dct))
+    db.githubuser.insert_many([dct])
