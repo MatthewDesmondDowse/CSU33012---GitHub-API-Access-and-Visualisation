fa135c11737479428b5faada735b9ddd39773199
Matthew Dowse <dowsema@tcd.ie>
2022-07-28 17:10:10 +0100

    Update script.py
    Enter token, displays info of user and their public repositories.
    Enter one of their repositories and it returns the breakdown of the languages used in that repo. Currently doesn't work for organization repos.

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 #this script accesses the GitHub API
 
 #import Github from the PyGithub library
+from urllib.request import urlopen
+import webbrowser
 from github import Github   #GitHub API access
 import json                 
 import pymongo  
 import os
+import requests
+import pprint
+
 
 from faker import Faker
 from collections import defaultdict
@@ -29,7 +34,9 @@ dct = {'user': names[user.login].replace(" ", ""),
        'fullname' : names[user.name],
        'location': user.location,
        'company': user.company,
-       'public_repos': user.public_repos
+       'public_repos': user.public_repos,
+       'created_at' : str(user.created_at),
+       'updated_at' : str(user.updated_at)
        }
 
 print ("dictionary is " + json.dumps(dct))
@@ -55,21 +62,32 @@ db.githubuser.insert_many([dct])
 
 ##demo
 
-followercount = user.followers
-print("followers: " + str(followercount))
+for repo in user.get_repos():
+    print(repo.full_name)
+    
+a = 1
+while a == 1:   
+    repoName = input("Enter name of repo to find language breakdown: ")
+    url = "https://api.github.com/repos/{}/{}/languages".format(user.login, repoName)
+    print(requests.get(url).text)
+    
+    continue
 
-followercount = user.get_followers()
+# followercount = user.followers
+# print("followers: " + str(followercount))
 
-for f in followercount:
-    dct = {'user': names[f.login].replace(" ", ""),
-           'fullname': names[f.name],
-           'location': f.location,
-           'company': f.company,
-           'public_repos': f.public_repos
-          }
-    for k, v in dict(dct).items():
-        if v is None:
-            del dct[k]
+# followercount = user.get_followers()
+
+# for f in followercount:
+#     dct = {'user': names[f.login].replace(" ", ""),
+#            'fullname': names[f.name],
+#            'location': f.location,
+#            'company': f.company,
+#            'public_repos': f.public_repos
+#           }
+#     for k, v in dict(dct).items():
+#         if v is None:
+#             del dct[k]
                 
-    print("follower: " + json.dumps(dct))
-    db.githubuser.insert_many([dct])
+#     print("follower: " + json.dumps(dct))
+#     db.githubuser.insert_many([dct])
