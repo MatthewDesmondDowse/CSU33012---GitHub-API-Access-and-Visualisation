ad09a60e7baf20bba62830ebfbd5454ae497be29
Matthew Dowse <dowsema@tcd.ie>
2022-08-21 12:01:58 +0100

    Update script.py
    Gets all contributors by going through 2 pages of data, 100 per page.
    Gets SHA commit number for each commit.

--- script.py
+++ script.py
@@ -30,29 +30,77 @@ client = pymongo.MongoClient(conn)
 #Create a Database
 db = client.classDB
 
-#create list to store all contributors of d3 repo
+#####################################################################
+#create list to store all NAMES OF CONTRIBUTORS of d3 repo
 contributors = []  
-contributors_Url = f"https://api.github.com/repos/d3/d3/contributors"    
+contributors_Url = f"https://api.github.com/repos/d3/d3/contributors?per_page=100"    
 response = requests.get(contributors_Url)
 data = response.json()
 
 for repoC in data:
-    contributors.insert(1,repoC["login"])
+    contributors.insert(0,repoC["login"])
+
+#GitHub only shows 100 contributors per page so we have to go to second page
+#we know d3 has 123 contributors from GitHub,
+#This counts 120, possible deleted accounts    
+contributors_Url_page2 =f"https://api.github.com/repos/d3/d3/contributors?page=2&per_page=100"    
+response = requests.get(contributors_Url_page2)
+data2 = response.json()
+
+for repoC in data2:
+    contributors.insert(0,repoC["login"])
 
 print(contributors , len(contributors))   
 
-#Get creation date
+######################################################################
+#Get CREATION DATE
 d3_url = f"https://api.github.com/repos/d3/d3"
 response = requests.get(d3_url)
-data = response.json()
+data3 = response.json()
 
-created_date = data["created_at"]
+created_date = data3["created_at"]
     
-print(created_date)        
+print(created_date)  
 
+#####################################################################
+#Get SHA COMMIT HISTORY  
+#From my own manual testing with URL queries I know there are 
+#44 pages of commits when the page displays 100 commits per page   
+i = 1
+sha_list = []
+commits_url =f"https://api.github.com/repos/d3/d3/commits?per_page=100&page={i}"
 
+while i <=1 :   #change to 45 later on, issues with api access 
+    response = requests.get(commits_url)
+    data4 = response.json()
 
+    for sha in data4:
+        sha_list.insert(1,sha["sha"])
 
+    i = i +1
+
+print(sha_list, len(sha_list))
+
+###################################################################
+
+#Take list of sha and search for each url with sha,
+#take login name, date comitted, and number of changes made
+
+# commit_info = []
+# commit_info_url ="https://api.github.com/repos/d3/d3/commits/" 
+# for t in sha_list:
+#     response = requests.get(f"{commit_info_url}/{t}")
+#     data5 = response.json()
+#     print(f"{commit_info_url}/{t}")
+#     for x in data5:
+#         commit_info.insert(0,x["login"])
+#         for y in data5:
+#             commit_info.insert(0,x["date"])
+#             for z in data5:
+#                 commit_info.insert(0,x["changes"])
+
+# print(commit_info)
+####################################################################
 
 
 ###-----------------------------------###    
